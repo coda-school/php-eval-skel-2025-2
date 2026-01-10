0172dab7e3ac427d4c14a568d1e238d789bfc3ae
croussey10 <charlie.roussey@coda-student.school>
2026-01-10 17:26:30 +0100

    feat: entity Likes

---
+++ src/Entity/Likes.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LikesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LikesRepository::class)]
+class Likes
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    private ?Tweets $tweet = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTweet(): ?Tweets
+    {
+        return $this->tweet;
+    }
+
+    public function setTweet(?Tweets $tweet): static
+    {
+        $this->tweet = $tweet;
+
+        return $this;
+    }
+}
