81200e16a53dd40feb6864f05a18da5103ff441d
jnarr <jn.arriero@gmail.com>
2026-01-13 13:25:37 +0100

    feat: début feature modif tweet

--- src/Controller/Tweets/EditController.php
+++ src/Controller/Tweets/EditController.php
@@ -2,14 +2,24 @@
 
 namespace App\Controller\Tweets;
 
+use App\DTO\TweetDTO;
+use App\Entity\Tweets;
+use App\Service\TweetsService;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class EditController extends AbstractController
 {
     #[Route('/tweets/{uid}/edit', name: 'tweets_edit', methods: ['GET', 'POST'])]
-    public function index(): Response
+    public function index(
+        Request $request,
+        #[MapEntity(mapping: ['uid' => 'uid'])]
+        Tweets $tweets,
+        TweetsService $tweetsService
+    ): Response
     {
         return $this->render('tweets/edit/index.html.twig', [
             'controller_name' => 'Tweets/EditController',
--- src/DTO/TweetDTO.php
+++ src/DTO/TweetDTO.php
@@ -3,9 +3,22 @@
 namespace App\DTO;
 
 
+use App\Entity\Tweets;
+
 class TweetDTO
 {
 
     public string $message;
+
+    public static function fromEntity(Tweets $tweets): TweetDTO
+    {
+        // on crée une nouvelle instance de la DTO
+        $dto = new self();
+
+        // on remplit les propriétés de la DTO avec les données de l'entité
+        $dto->message = $tweets->getMessage();
+
+        return $dto;
+    }
 
 }
