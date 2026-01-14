f2c54f1a6257eb33bcf06f30ad159324b1ea71cb
CDasse <clemence.dasse@yahoo.fr>
2026-01-14 17:00:57 +0100

    Refactor: déplace logique métier dans service

--- src/Service/FollowsService.php
+++ src/Service/FollowsService.php
@@ -4,18 +4,44 @@ namespace App\Service;
 
 
 use App\Entity\Follows;
+use App\Entity\User;
 use App\Repository\FollowsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class FollowsService
 {
     public function __construct(
         private readonly FollowsRepository $followsRepository,
+        private readonly EntityManagerInterface $em,
     )
     {
     }
 
     public function findIfFollowerFollowFollowed (string $followerName, string $followedName): ?Follows {
         return $this->followsRepository->findIfFollowerFollowFollowed($followerName, $followedName);
+    }
+
+    public function toggleFollow(User $currentUser, User $userToFollow): void
+    {
+        $isFollowed = $this->followsRepository->findIfFollowerFollowFollowed(
+            $currentUser->getUsername(),
+            $userToFollow->getUsername()
+        );
+
+        if ($isFollowed) {
+            $isFollowed->setIsDeleted(true);
+            $isFollowed->setDeletedBy($currentUser);
+            $isFollowed->setDeletedDate(new \DateTime());
+        } else {
+            $isFollowed = new Follows();
+            $isFollowed->setCreatedBy($currentUser);
+            $isFollowed->setCreatedDate(new \DateTime());
+            $isFollowed->setFollowed($userToFollow);
+            $isFollowed->setFollower($currentUser);
+            $this->em->persist($isFollowed);
+        }
+
+        $this->em->flush();
     }
 
 }
