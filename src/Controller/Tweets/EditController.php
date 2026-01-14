980396e5a8df6623ccee54f8c73cacf4ee0d3a99
jnarr <jn.arriero@gmail.com>
2026-01-14 16:54:52 +0100

    feat: ajout de la modification des tweets + consultation des tweets sur page unique

--- src/Controller/Tweets/EditController.php
+++ src/Controller/Tweets/EditController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Tweets;
 
 use App\DTO\TweetDTO;
 use App\Entity\Tweets;
+use App\Form\TweetType;
 use App\Service\TweetsService;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,8 +22,34 @@ final class EditController extends AbstractController
         TweetsService $tweetsService
     ): Response
     {
+        $connectedUser = $this->getUser();
+
+        if ($tweets->getCreatedBy() !== $connectedUser) {
+            throw $this->createAccessDeniedException("Ce n'est pas votre tweet !");
+        }
+
+        $dto = TweetDTO::fromEntity($tweets);
+        $form =$this->createForm(TweetType::class, $dto);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $dto = $form->getData();
+
+            try {
+                $tweets = $tweetsService->updateTweet($tweets, $dto, $connectedUser);
+            } catch (\Exception $e) {
+                $this->addFlash('danger', 'Erreur lors de la modification du tweet');
+
+                return $this->redirectToRoute('tweets_edit', ['uid' => $tweets->getUid()]);
+            }
+
+            $this->addFlash('success', 'Success modification du tweet!');
+
+            return $this->redirectToRoute('tweets_show', ['uid' => $tweets->getUid()]);
+        }
+
         return $this->render('tweets/edit/index.html.twig', [
-            'controller_name' => 'Tweets/EditController',
+            'form' => $form,
+            'tweets' => $tweets,
         ]);
     }
 }
--- src/Controller/Tweets/ShowController.php
+++ src/Controller/Tweets/ShowController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller\Tweets;
 
+use App\Entity\Tweets;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+
 
 final class ShowController extends AbstractController
 {
     #[Route('/tweets/{uid}', name: 'tweets_show', methods: ['GET'])]
-    public function index(): Response
+    public function index(
+        #[MapEntity(mapping: ['uid' => 'uid'])]
+        Tweets $tweet,
+    ): Response
     {
         return $this->render('tweets/show/index.html.twig', [
-            'controller_name' => 'Tweets/ShowController',
+            'tweet' => $tweet,
         ]);
     }
 }
