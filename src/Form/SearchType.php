f89dac5e96016c1f569efdea5e97a5fe4585a884
CDasse <clemence.dasse@yahoo.fr>
2026-01-24 12:34:53 +0100

    Fix: block search with no character or blank

--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class SearchType extends AbstractType
 {
@@ -16,9 +17,14 @@ class SearchType extends AbstractType
         $builder
             ->add('rechercher', TextType::class, [
                 'constraints' => [
+                    new NotBlank(
+                        message: 'Vous devez entrer une recherche.'
+                    ),
                     new Length(
+                        min: 1,
                         max: 50,
-                        maxMessage: "Votre recherche ne peut pas dépasser 50 caractères",
+                        minMessage : "Votre recherche doit contenir au minimum 1 caractère.",
+                        maxMessage: "Votre recherche ne peut pas dépasser 50 caractères.",
                     )
                 ],
                 'required' => false,
